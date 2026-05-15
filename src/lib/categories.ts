eac608e468cae03d61459badffe7c2aba1ce5b1a
madegwa-o <madegwaoscar317@gmail.com>
2026-05-15 17:49:57 +0300

    rework

--- src/lib/categories.ts
+++ src/lib/categories.ts
@@ -1,6 +1,6 @@
 export enum MainCategory {
-    VEHICLES = "vehicles",
-    CONSTRUCTION_FREELANCERS = "construction-freelancers",
-    CAREERS = "careers",
-    PROPERTIES = "properties",
+    VEHICLES = "VEHICLES",
+    CONSTRUCTION_FREELANCERS = "CONSTRUCTION_FREELANCERS",
+    CAREERS = "CAREERS",
+    PROPERTIES = "PROPERTIES",
 }
